3e4ef1c17487f622fe261f753b547c4169b10f65
lvkt0177 <lvkt0177@gmail.com>
2024-12-30 23:11:45 +0700

    Dự án ASP.NET (Cập nhật Model) - Thinh

---
+++ DoAnWEBDEMO/Models/LienHe.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DoAnWEBDEMO.Models
+{
+    public class LienHe
+    {
+        [Key]
+        public int MA_LH { get; set; }  
+
+        public string MA_NV { get; set; }  
+
+        [Required]
+        [StringLength(255)]
+        public string HO_TEN { get; set; }  
+
+        [EmailAddress]
+        [StringLength(255)]
+        public string EMAIL { get; set; } 
+
+        [StringLength(11)]
+        public string SDT { get; set; }  
+
+        [StringLength(255)]
+        public string NOI_DUNG { get; set; }  
+
+        [DataType(DataType.DateTime)]
+        public DateTime THOI_GIAN_GUI { get; set; } 
+
+        public int TRANG_THAI { get; set; }  
+    }
+}
