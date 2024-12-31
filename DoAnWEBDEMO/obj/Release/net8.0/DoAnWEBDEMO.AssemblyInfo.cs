51ec517d5dfcf702e33af6aed8760235758941c3
ducmanh13112004 <tmanh5417@gmail.com>
2024-12-31 13:41:25 +0700

    moi

--- DoAnWEBDEMO/obj/Release/net8.0/DoAnWEBDEMO.AssemblyInfo.cs
+++ DoAnWEBDEMO/obj/Release/net8.0/DoAnWEBDEMO.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnWEBDEMO")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ac596b6f02432808da53087c7c11e910d547f22")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnWEBDEMO")]
