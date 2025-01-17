c511d8af65640fafbc6baa4348137c0d4932ada1
lvkt0177 <164190465+lvkt0177@users.noreply.github.com>
2025-01-17 11:56:28 +0700

    Cập nhật Model Header, Footer, DichVuCongTy

---
+++ DoAnWEBDEMO/Models/Footer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DoAnWEBDEMO.Models
+{
+    public class Footer
+    {
+        [Key]
+        public int ID { get; set; }
+        public string? TieuDe { get; set; }
+        public string? MoTa { get; set; }
+        public string? HinhAnh { get; set; }
+        public string? DuongLienKet { get; set; }
+        [Required]
+        public int? TrangThaiHienThi { get; set; }
+
+    }
+}
